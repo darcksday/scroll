ba8fae66554ea7bab393e2ed45b0a5e2d8405534
darcksday <darcksday@gmail.com>
2024-10-22 16:11:06 +0300

    small fix

--- modules/contracts/functions.py
+++ modules/contracts/functions.py
@@ -281,6 +281,9 @@ def pump_claim_data(wallet):
 def claim_scroll(web3, private_key, _amount=0):
     wallet =web3.to_checksum_address( web3.eth.account.from_key(private_key).address)
     data=scroll_claim_data(wallet)
+    if not data:
+        raise Exception(f'SKIP. Not Eglible')
+
     if not data['claimed_at']:
         amount=int(data['amount'])
         proof= data['proof']
